601eefb31e1a928b7b5cc14fad8e1c7e724188a6
Elizabeth <elizabethlagos1998@gmail.com>
2026-05-29 11:49:25 -0400

    Subida inicial del proyecto Administrador con pom.xml

---
+++ src/test/java/cl/duoc/administrador/AdministradorApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.administrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
